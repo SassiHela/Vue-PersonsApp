23f5e535cfae4334435e6a40b3515c2ad0bd02b5
Hela Sassi <hela.sassi13@gmail.com>
2021-10-10 12:13:30 +0100

    update server.js

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
-
-
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
